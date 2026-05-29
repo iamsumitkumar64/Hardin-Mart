1c58309245681ca5fa67b650b29dddba768dbad7
Sumit Kumar <iam.sumitkumar64@gmail.com>
2026-05-29 17:24:02 +0530

    stages fixed

--- frontend/src/app/order/page.tsx
+++ frontend/src/app/order/page.tsx
@@ -49,9 +49,8 @@ export default function OrderPage() {
     const orderSteps = [
         OrderStatusEnum.PLACED,
         OrderStatusEnum.BILLED,
-        OrderStatusEnum.SHIPPED,
-        OrderStatusEnum.DELIVERED,
-    ];
+        OrderStatusEnum.READY_TO_SHIP,
+        OrderStatusEnum.CANCELLED,];
 
     const getActiveStep = (status: OrderStatusEnum) => {
         return orderSteps.indexOf(status);
@@ -100,7 +99,7 @@ export default function OrderPage() {
                                             <Step
                                                 key={step}
                                                 completed={
-                                                    shipmentOrder?.order_status === OrderStatusEnum.DELIVERED
+                                                    shipmentOrder?.order_status === OrderStatusEnum.READY_TO_SHIP
                                                         ? true
                                                         : undefined
                                                 }
